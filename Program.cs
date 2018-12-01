2553777f6bd7675d61628c9660602c0c467f5c57
Peter Halasz <peter@becauseofgames.com>
2018-12-01 17:12:01 +1100

    Condense down redundant code

--- Program.cs
+++ Program.cs
@@ -6,40 +6,9 @@ public class Program {
         string b = "0:00:00.01000000";  // 8 decimal digits
         string c = "0:00:00.010000000"; // 9 decimal digits
 
-        TimeSpan aTimeSpan;
-        TimeSpan bTimeSpan;
-        TimeSpan cTimeSpan;
-
-        string aText;
-        string bText;
-        string cText;
-
-        try {
-            aTimeSpan = System.TimeSpan.Parse(a);
-            aText = aTimeSpan.TotalMilliseconds + " ms";
-        } catch (System.OverflowException) {
-            aText = "OverflowException";
-        } catch (System.FormatException) {
-            aText = "FormatException";
-        }
-
-        try {
-            bTimeSpan = System.TimeSpan.Parse(b);
-            bText = bTimeSpan.TotalMilliseconds + " ms";
-        } catch (System.OverflowException) {
-            bText = "OverflowException";
-        } catch (System.FormatException) {
-            bText = "FormatException";
-        }
-
-        try {
-            cTimeSpan = System.TimeSpan.Parse(c);
-            cText = cTimeSpan.TotalMilliseconds + " ms";
-        } catch (System.OverflowException) {
-            cText = "OverflowException";
-        } catch (System.FormatException) {
-            cText = "FormatException";
-        }
+        string aText = TimeSpanParseToString(a);
+        string bText = TimeSpanParseToString(b);
+        string cText = TimeSpanParseToString(c);
 
         string version = Environment.Version.ToString();
         string assemblyFullName = typeof(TimeSpan).Assembly.FullName;
@@ -48,5 +17,18 @@ public class Program {
         Console.WriteLine("| version | `{0}` | `{1}` | `{2}` | Assembly.FullName | ImageRuntimeVersion | Notes |", a, b, c);
         Console.WriteLine("| --- | --- | --- | --- | --- | --- | --- |");
         Console.WriteLine("| {0} | `{1}` | `{2}` | `{3}` | {4} | {5} | | ", version, aText, bText, cText, assemblyFullName, imageRuntimeVersion);
+    }
+
+    public static string TimeSpanParseToString(string spanString) {
+        try {
+            TimeSpan timespan = System.TimeSpan.Parse(spanString);
+            return timespan.TotalMilliseconds + " ms";
+        } catch (System.OverflowException) {
+            return "OverflowException";
+        } catch (System.FormatException) {
+            return "FormatException";
+        }
+
+        return "???";
     }
 }
